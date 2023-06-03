7cc7932ae955b887216b6ceb8234450e07014748
Rafał Łukawski <rs.lukawski@gmail.com>
2023-06-03 07:06:51 +0200

    complete navigation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,9 @@
 import { type NextPage } from "next";
 import { api } from "~/utils/api";
-import { Text, Heading, HStack, Grid, GridItem, Box } from "@chakra-ui/react";
+import { Text, Heading, HStack, Grid, GridItem, Box, Button } from "@chakra-ui/react";
 import { HiBars3 } from "react-icons/hi2";
-
+import { BsBell } from "react-icons/bs";
+import { AiOutlineEdit } from "react-icons/ai";
 
 const Home: NextPage = () => {
   const hello = api.example.hello.useQuery({ text: "from tRPC" });
@@ -14,10 +15,14 @@ const Home: NextPage = () => {
       gridTemplateColumns={'8fr 4fr'}
       gap="2">
       <GridItem area='header' bgColor="lightgreen">
-        <HStack justifyContent='space-between'>
+        <HStack justifyContent='space-between' p='3'>
           <HiBars3 size="1.5em" />
           <Heading>This is Header</Heading>
-          <Text>3</Text>
+          <HStack columnGap="4">
+            <BsBell size="1.5em" />
+            <Box bgColor="gray.600" w="5" h="5" borderRadius="full"></Box>
+            <Button variant="outline" colorScheme="dark" columnGap="2"><Text>Write</Text><AiOutlineEdit /></Button>
+          </HStack>
         </HStack>
       </GridItem>
       <GridItem area='body' bgColor="lightblue">
