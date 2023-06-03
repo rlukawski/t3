73702024427f2486833fcb71bb9c54facd5d7cff
Rafał Łukawski <rs.lukawski@gmail.com>
2023-06-03 06:40:46 +0200

    update chakra layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,27 @@
 import { type NextPage } from "next";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { api } from "~/utils/api";
-import { VStack, Container, Text } from "@chakra-ui/react";
+import { VStack, Container, Text, Heading, HStack, Grid, GridItem } from "@chakra-ui/react";
 
 const Home: NextPage = () => {
   const hello = api.example.hello.useQuery({ text: "from tRPC" });
 
   return (
-    <Container>
-      <VStack w="full"><Text>Hello from the world of Next.js</Text></VStack>
-    </Container>
+    <Grid templateAreas={`"header header"
+                           "body sidebar"`}
+      gridTemplateRows={'1fr 1fr'}
+      gridTemplateColumns={'8fr 4fr'}
+      gap="2">
+      <GridItem area='header' bgColor="lightgreen">
+        <Heading>This is Header</Heading>
+      </GridItem>
+      <GridItem area='body' bgColor="lightblue">
+        <Text>Hello from the world of Next.js</Text>
+      </GridItem>
+      <GridItem area='sidebar' bgColor="yellow">
+        <Text>Sidebar</Text>
+      </GridItem>
+    </Grid>
   );
 };
 
